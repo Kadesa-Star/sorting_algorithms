e1d4c180aedca97f0d5685553e3cde80bf82f4b8
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-29 05:18:38 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,7 +14,8 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	int swapped;
 
-	if (array == NULL || size == 0)
+
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
